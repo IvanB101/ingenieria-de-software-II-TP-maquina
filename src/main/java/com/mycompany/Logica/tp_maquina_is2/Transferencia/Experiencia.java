b24153cdb999165fddeef40e6114258541548208
NoCaffeineNoCode <ivanbrocas01@gmail.com>
2022-10-04 10:07:37 -0300

    Reestructuracion carpetas

--- src/main/java/com/mycompany/Logica/tp_maquina_is2/Transferencia/Experiencia.java
+++ src/main/java/com/mycompany/Logica/tp_maquina_is2/Transferencia/Experiencia.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Logica.Util;
+package com.mycompany.Logica.tp_maquina_is2.Transferencia;
 
 /**
  *
  * @author ivanb
  */
-public class Excel {
-    
+public class Experiencia {
+    private int dificultad;
+    private int dias;
+    private int dedicacion;
+    private Examen examen;
 }
--- src/main/java/com/mycompany/Logica/tp_maquina_is2/Util/Excel.java
+++ src/main/java/com/mycompany/Logica/tp_maquina_is2/Util/Excel.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Logica.Transferencia;
+package com.mycompany.Logica.tp_maquina_is2.Util;
 
 /**
  *
  * @author ivanb
  */
-public class Experiencia {
-    private int dificultad;
-    private int dias;
-    private int dedicacion;
-    private Examen examen;
+public class Excel {
+    
 }
