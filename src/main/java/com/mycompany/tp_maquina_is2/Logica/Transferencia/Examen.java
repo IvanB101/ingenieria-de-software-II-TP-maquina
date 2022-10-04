544fe4af92395c3505ce9373242776b0add20142
NoCaffeineNoCode <ivanbrocas01@gmail.com>
2022-10-04 10:12:43 -0300

    reestructuracion carpetas

--- src/main/java/com/mycompany/tp_maquina_is2/Logica/Transferencia/Examen.java
+++ src/main/java/com/mycompany/tp_maquina_is2/Logica/Transferencia/Examen.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.Logica.tp_maquina_is2.Transferencia;
+package com.mycompany.tp_maquina_is2.Logica.Transferencia;
 
 import java.util.Date;
 
