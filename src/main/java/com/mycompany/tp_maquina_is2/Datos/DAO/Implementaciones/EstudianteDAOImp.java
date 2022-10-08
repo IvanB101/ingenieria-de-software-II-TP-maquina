d8159aa604bc5cb8862e378edc33dda59b7bee34
NoCaffeineNoCode <ivanbrocas01@gmail.com>
2022-10-07 22:09:46 -0300

    Inicio implementacion DAO

--- src/main/java/com/mycompany/tp_maquina_is2/Datos/DAO/Implementaciones/EstudianteDAOImp.java
+++ src/main/java/com/mycompany/tp_maquina_is2/Datos/DAO/Implementaciones/EstudianteDAOImp.java
@@ -4,19 +4,55 @@
  */
 package com.mycompany.tp_maquina_is2.Datos.DAO.Implementaciones;
 
+import com.mycompany.tp_maquina_is2.Datos.Conexion;
 import com.mycompany.tp_maquina_is2.Datos.DAO.Interfaces.EstudianteDAOInter;
 import com.mycompany.tp_maquina_is2.Logica.Transferencia.Estudiante;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ivanb
  */
-public class EstudianteDAOImp implements EstudianteDAOInter{
+public class EstudianteDAOImp implements EstudianteDAOInter {
+
+    Connection con;
+
+    public EstudianteDAOImp(Conexion conexion) {
+        try {
+            con = conexion.getInstance();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
 
     @Override
     public boolean create(Estudiante estudiante) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Persona (codigo, dni, nombre, apellido) VALUES (?,?,?,?)");
+
+            ps.setInt(1, estudiante.getCodigo());
+            ps.setInt(2, estudiante.getDni());
+            ps.setString(3, estudiante.getNombre());
+            ps.setString(4, estudiante.getApellido());
+
+            ps.executeUpdate();
+            
+            ps = con.prepareStatement("INSERT INTO Estudiante (nroRegistro, Persona_codigo) VALUES (?,?)");
+            
+            ps.setInt(1, estudiante.getNroRegistro());
+            ps.setInt(2, estudiante.getCodigo());
+            
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -33,5 +69,5 @@ public class EstudianteDAOImp implements EstudianteDAOInter{
     public boolean delete(int nroRegistro) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
